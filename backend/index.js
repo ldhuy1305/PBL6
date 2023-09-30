2bc9e52a792fa3aa1ba5ab7448fd0e94b0ea21e1
honganh1903 <lehonganh1903@gmail.com>
2023-09-30 14:54:58 +0700

    fix 30/9

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,35 @@
 // Import
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 //Configuring the database
-const dbConfig = 'mongodb://localhost:27017';
-const mongoose = require('mongoose');
+const dbConfig = "mongodb://localhost:27017";
+const dotenv = require("dotenv");
 
+dotenv.config({ path: "./config.env" });
 // Connecting to the database
-console.log("connecting to", dbConfig);
-mongoose
-  .connect(dbConfig)
-  .then(() => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+const DB = process.env.DATABASE;
 
-
- 
 const app = express();
-app.use(express.urlencoded({
-    extended : true,
-}))
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
 
 // route
 route(app);
 
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("DB connection successful!"));
+
 port = process.env.PORT || 3000;
-app.listen(port,() => {
-    console.log(`http://localhost:${port}`);
-})
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
