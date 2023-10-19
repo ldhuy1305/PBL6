22f2727f50d3ba74d55d3b7d9e64c898feb1a13a
Đức Huy Lê <bongmaxuyentuong1@gmail.com>
2023-10-19 10:56:28 +0700

    fix convention 19/10

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,7 +3,7 @@ const catchAsync = require("../utils/catchAsync");
 const Category = require("../models/category");
 const handleController = require("./handleController");
 const ApiFeatures = require("../utils/ApiFeatures");
-const AppError = require("../utils/AppError");
+const appError = require("../utils/appError");
 class ProductController {
   // get All Product by Store
   getAllProductByStore = catchAsync(async (req, res, next) => {
@@ -30,7 +30,7 @@ class ProductController {
     let cat = await Category.findOne({
       catName: req.body.catName,
     });
-    if (!cat) return new AppError("Không tìm thấy tên danh mục", 404);
+    if (!cat) return new appError("Không tìm thấy tên danh mục", 404);
     const product = await Product.create(req.body);
     res.status(201).json({
       status: "success",
@@ -43,7 +43,7 @@ class ProductController {
   deleteProduct = catchAsync(async (req, res, next) => {
     const product = await Product.findByIdAndDelete({ _id: req.params.id });
     if (!product) {
-      return next(new AppError("Không thể tìm thấy sản phẩm", 404));
+      return next(new appError("Không thể tìm thấy sản phẩm", 404));
     }
     res.status(201).json("Đã xoá thành công");
   });
