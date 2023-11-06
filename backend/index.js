a717111303dee16a4456e7028c8643dbb952d395
ldhuy1305 <bongmaxuyentuong1@gmail.com>
2023-11-06 21:46:09 +0700

    remove moment

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,6 @@ const session = require("express-session");
 const MemoryStore = require("memorystore")(session);
 const passport = require("passport");
 const { fileParser } = require("express-multipart-file-parser");
-const moment = require("moment-timezone");
 require("./utils/googleAuth");
 
 const rateLimit = require("express-rate-limit");
@@ -17,8 +16,6 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const hsts = require("hsts");
 dotenv.config({ path: "./.env" });
-// Connecting to the database
-moment.tz.setDefault("Asia/Ho_Chi_Minh");
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
