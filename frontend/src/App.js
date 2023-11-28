60093ca09b6f35ab08beeca07a3cbcd4010464aa
HuynhBaThuan <thuanhuynh.260702@gmail.com>
2023-11-28 09:38:26 +0700

    update signUp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,6 @@ import Store from './Store/Store';
 import Admin from './Admin/App';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Notify from './User/Components/Notify.jsx/Notify';
-import { withRouter } from 'react-router-dom';
-import { faL } from '@fortawesome/free-solid-svg-icons';
 function App() {
   const [openNotify, setOpenNotify] = useState(false)
   const UserRoute = ({ element, role }) => {
