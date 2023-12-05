5a94b6d2ddc32d9250e222574a86a6c1bd0ad80f
ldhuy1305 <bongmaxuyentuong1@gmail.com>
2023-12-05 13:57:38 +0700

    fix change status

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -244,7 +244,10 @@ class orderController {
     if (!order) return next(new appError("Không tìm thấy đơn hàng"), 404);
     let message;
     // when shipper accept order
-    if (order.status == "Pending") {
+
+    if (order.shipper != shipperId && order.shipper)
+      return next(new appError("Đã xuất hiện lỗi khi giao hàng"), 404);
+    if (order.status == "Waiting") {
       order.shipper = shipperId;
       order.status = "Preparing";
       message = "Shipper đã xác nhận giao hàng";
