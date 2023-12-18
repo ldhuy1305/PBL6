94d6c10011e8913f1d4cdb4ddd0605787122ec94
HuynhBaThuan <thuanhuynh.260702@gmail.com>
2023-12-18 09:03:26 +0700

    fix login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,7 @@
-import React, {useState, useEffect } from 'react';
 import User from './User/App';
 import Store from './Store/Store';
 import Admin from './Admin/App';
-import Shipper from './Shipper/shipper'
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   const UserRoute = ({ element, role }) => {
@@ -22,9 +20,7 @@ function App() {
               return (<Admin/>)
             } else if (userData.role === 'Owner') {
               return (<Store/>)
-            } else if (userData.role === 'Shipper') {
-              return (<Shipper/>)
-            } 
+            }  
       }
     } else {
       return (<User />);
@@ -34,7 +30,6 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/shipper" element={<UserRoute element={<Shipper />} role="Shipper" />} />
         <Route path="/*" element={<UserRoute element={<User />} role="User" />} />
         <Route path="/store/*" element={<UserRoute element={<Store />} role="Owner" />} />
         <Route path="/admin/*" element={<UserRoute element={<Admin />} role="Admin" />} />
