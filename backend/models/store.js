7607de02fce983ee6bfd430ce093e6717f63487f
ldhuy1305 <bongmaxuyentuong1@gmail.com>
2024-01-03 22:37:23 +0700

    fix model store

--- backend/models/store.js
+++ backend/models/store.js
@@ -86,18 +86,18 @@ const storeSchema = new Schema(
     toObject: { virtuals: true },
   }
 );
-storeSchema.pre("findOneAndUpdate", { query: true }, async function(next) {
-  const update = this.getUpdate();
-  if (update.address) {
-    const loc = await mapUtils.getGeoCode(update.address);
-    this._update.location = {
-      type: "Point",
-      coordinates: [loc[0].latitude, loc[0].longitude],
-    };
-    console.log(update.location);
-  }
-  next();
-});
+// storeSchema.pre("findOneAndUpdate", { query: true }, async function(next) {
+//   const update = this.getUpdate();
+//   if (update.address) {
+//     const loc = await mapUtils.getGeoCode(update.address);
+//     this._update.location = {
+//       type: "Point",
+//       coordinates: [loc[0].latitude, loc[0].longitude],
+//     };
+//     console.log(update.location);
+//   }
+//   next();
+// });
 storeSchema.pre("save", async function(next) {
   if (this.isNew || this.isModified("address")) {
     const loc = await mapUtils.getGeoCode(this.address);
